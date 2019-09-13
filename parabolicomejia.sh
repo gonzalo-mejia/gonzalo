b2b87246aa1ae98e89efb077fb3cffbc1681fa45
gonzalo-mejia <55259712+gonzalo-mejia@users.noreply.github.com>
2019-09-13 11:45:50 -0500

    Update parabolicomejia.sh

--- parabolicomejia.sh
+++ parabolicomejia.sh
@@ -1,5 +1,5 @@
 for i  in $(seq 1 20);do
-	echo |awk -v n=$i '{print 1*cos(45)*n , (1*sin(45)*n)+(4.9*(n*n))}'
+	echo |awk -v n=$i '{print 1*cos(45)*n , (1*sin(45)*n)-(4.9*(n*n))}'
 done
 
-computacional mejia
+
